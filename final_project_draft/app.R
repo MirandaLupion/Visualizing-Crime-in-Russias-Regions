8dfe117253f83db0acb3e4baa6f9444c93390253
Miranda Lupion <lupion@g.harvard.edu>
2018-10-21 09:59:08 -0400

    Issues with output

---
+++ final_project_draft/app.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(tidyverse)
+load("Regions4.rda")
+r_4 <- da35355.0004
+r_4$REGION <- str_trim(r_4$REGION)
+r_4$NAME <- str_trim(r_4$NAME)
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Crime in the Russian Regions, 1990 - 2010"),
+   
+   sidebarLayout( 
+     sidebarPanel( 
+       selectInput(inputId = "y", #internal label 
+                   label = "Y-axis:", #label that user sees
+                   choices = c("Road accidents" = "ROADACCIDENT", "Crime share" = "CRIMESHARE"), #vector of choices for user to pick from 
+                  selected = "ROADACCIDENT"),
+       
+      selectizeInput(inputId = "region",
+                          label = "Select regions", 
+                          choices = c(r_4$REGION),
+                          multiple = TRUE,
+                          options = list(maxItems = 5))),
+     mainPanel(
+       plotOutput(outputId = "scatterplot"))))
+
+# Server
+server <- function(input, output){
+  regions_subset <- reactive({
+    req(input$region)
+    filter(r_4, REGION %in% input$region)
+  })
+  output$scatterplot <- renderPlot({
+    ggplot(data = regions_subset(), aes_string(x = r_4$YEAR, y = input$y, color = input$region)) + geom_point() 
+  })}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
