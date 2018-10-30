b1315dff3a61e8f500bc54067eb99070c86daf17
Miranda Lupion <lupion@g.harvard.edu>
2018-10-29 22:38:23 -0400

    edits

--- final_project_draft/app.R
+++ final_project_draft/app.R
@@ -1,13 +1,20 @@
 library(shiny)
 library(tidyverse)
 library(stringr)
-load("Regions4.rda")
-r_4 <- da35355.0004
-r_4$NAME <- str_trim(r_4$NAME)
+library(rsconnect)
+library(leaflet)
+library(rgdal)
 
+# Data preparation 
 
+crime <- read_rds("r_4_tidy.rds")
 
-# Define UI for application that draws a histogram
+# Leaflet preparation
+rf_map <- readOGR(dsn = "/Users/MLupion/Desktop/GOV 1005/GOV_1005_Final_Project/RUS_adm", layer = "RUS_adm1")
+
+rf_map <- spTransform(rf_map, CRS("+init=epsg:4326"))
+
+# Define UI for application that draws a graph
 ui <- fluidPage(
    
    # Application title
@@ -15,29 +22,51 @@ ui <- fluidPage(
    
    sidebarLayout( 
      sidebarPanel( 
+       # Y axis variable for the chart and for the map
        selectInput(inputId = "y", #internal label 
                    label = "Y-axis:", #label that user sees
-                   choices = c("Road accidents" = "ROADACCIDENT", "Crime share" = "CRIMESHARE", "Murders" = "MURDER"), #vector of choices for user to pick from 
+                   choices = c("Road accidents" = "ROADACCIDENT", 
+                               "Crime share" = "CRIMESHARE", 
+                               "Murders" = "MURDER"), #vector of choices for user to pick from 
                   selected = "ROADACCIDENT"),
+     
+      # for the map
+      selectInput(inputId = "year", #internal label 
+                   label = "Year to map", #label that user sees
+                   choices = c(crime$YEAR), #vector of choices for user to pick from 
+                   selected = "1990"),
        
-      selectizeInput(inputId = "region",
-                          label = "Select regions", 
-                          choices = c(r_4$NAME),
-                          multiple = TRUE,
-                          options = list(maxItems = 5))),
+        # Select regions 
+      selectizeInput(inputId = "region", #internal label
+                          label = "Select regions", #label that user sees
+                          choices = c(crime$NAME), #choose from this list 
+                          multiple = TRUE, # can choose multiple 
+                          options = list(maxItems = 5))), #can choose up to five
+     # Outputs 
      mainPanel(
-       plotOutput(outputId = "scatterplot"))))
+       plotOutput(outputId = "scatterplot"),
+       leafletOutput("map"),
+       p("Source - Inter-university Consortium for Political and Social Research (ICPSR):"),  
+       p("ICPSR 35355 Aggregate Data, Regions of Russia (RoR), 1990 - 2010, created by Irina Mirkina."),
+       tags$a(href = "https://www.icpsr.umich.edu/icpsrweb/ICPSR/studies/35355", "Click here to learn more about the data."))))
 
 # Server
 server <- function(input, output){
-  regions_subset <- reactive({
+  regions_subset <- reactive({ #make the regions included in the data set only the ones that the user chose 
     req(input$region)
-    filter(r_4, NAME %in% input$region)
+    filter(crime, NAME %in% input$region)
   })
   output$scatterplot <- renderPlot({
-    ggplot(data = regions_subset(), aes_string(x = regions_subset()$YEAR, y = input$y)) + 
-      geom_point(aes(color = regions_subset()$NAME)) +
-      labs(x = "Year", y = input$y)
+    ggplot(data = regions_subset(), aes_string(x = regions_subset()$YEAR, y = input$y)) + #plot year on x and value on y
+      geom_point(aes(color = regions_subset()$NAME)) + #color by region
+      labs(x = "Year", y = input$y) +
+      scale_color_discrete(name = "Regions")
+    
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(provider = "CartoDB") %>%
+      addPolygons(data = rf_map)
+  })  
   })}
 
 # Run the application 
